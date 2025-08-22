666a7d6d9838bf3743faadd5428ff222e5148244
Vladislav Sharkov <v.sharkov@planeta.ru>
2025-08-22 04:48:06 +0400

    vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,4 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3020,
+    host: true,
+    strictPort: true,
+    allowedHosts: ['reader.sharkovv.ru']
+  },
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    allowedHosts: ['reader.sharkovv.ru']
+  }
 })
